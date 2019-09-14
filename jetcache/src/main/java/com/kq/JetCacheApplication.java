9952ea8d7ebdad713e20599e79595455632e75c8
king <kongq1983@qq.com>
2019-09-14 08:29:46 +0800

    add jetcache

---
+++ jetcache/src/main/java/com/kq/JetCacheApplication.java
@@ -0,0 +1,17 @@
+package com.kq;
+
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableMethodCache(basePackages = "com.kq")
+@EnableCreateCacheAnnotation
+public class JetCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetCacheApplication.class);
+    }
+
+}
