fb260548975375a380350fdfc89719ff8a10f189
king <kongq1983@qq.com>
2019-11-17 01:03:20 +0800

    add resttemplate

--- httpclient/src/main/java/com/kq/httpclient/component/ResttemplateComponent.java
+++ httpclient/src/main/java/com/kq/httpclient/component/ResttemplateComponent.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.CountDownLatch;
+
 @Component
 public class ResttemplateComponent {
 
@@ -12,19 +14,32 @@ public class ResttemplateComponent {
 
     String url = "http://localhost:10001/helloworld";
 
+    public static final int size = 200;
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(size);
+
     public void start(){
 
-
-
-
         Runnable r = () -> {
-            String result = restTemplate.postForObject(url,null,String.class);
-            System.out.println(Thread.currentThread().getName()+"result="+result);
+            try {
+                String result = restTemplate.postForObject(url, null, String.class);
+                System.out.println(Thread.currentThread().getName() + "result=" + result);
+            }finally {
+                countDownLatch.countDown();
+            }
         };
 
-        for(int i=0;i<2000;i++) {
+        for(int i=0;i<size;i++) {
             new Thread(r,i+"").start();
         }
+
+        try{
+            countDownLatch.await();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("========================end============================");
 
     }
 
--- httpclient/src/main/java/com/kq/httpclient/config/MyConfiguration.java
+++ httpclient/src/main/java/com/kq/httpclient/config/MyConfiguration.java
@@ -1,22 +1,29 @@
 package com.kq.httpclient.config;
 
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MyConfiguration {
 
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         return new RestTemplate(factory);
     }
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+//        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+//        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create()
+                        .disableContentCompression()
+                        .setMaxConnPerRoute(5)
+                        .setMaxConnTotal(200).build());
         factory.setReadTimeout(5000);//单位为ms
         factory.setConnectTimeout(5000);//单位为ms
         return factory;
