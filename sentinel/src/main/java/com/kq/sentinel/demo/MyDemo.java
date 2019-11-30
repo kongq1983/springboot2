2d72ed8e64b0f75fef214184cae99048b3a6cdeb
king <king@qq.com>
2019-11-30 17:36:28 +0800

    add sentinel demo

---
+++ sentinel/src/main/java/com/kq/sentinel/demo/MyDemo.java
@@ -0,0 +1,18 @@
+package com.kq.sentinel.demo;
+
+/**
+ * MyDemo
+ *
+ * @author kq
+ * @date 2019-11-30
+ */
+public class MyDemo {
+
+    public static void main(String[] args) {
+        long num = 1575095617070l % 500;
+
+        System.out.println("num="+num);
+
+    }
+
+}
---
+++ sentinel/src/main/java/com/kq/sentinel/demo/SentinelCoreDemo.java
@@ -0,0 +1,58 @@
+package com.kq.sentinel.demo;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SentinelCoreDemo
+ * https://www.cnblogs.com/linjiqin/p/9707713.html
+ * @author kq
+ * @date 2019-11-28
+ */
+public class SentinelCoreDemo {
+
+    public static void main(String[] args) {
+        initFlowRules();
+//        int size = 1000000000;
+        int size = 1;
+
+//        while (true) {
+        for(int i=0;i<size;i++) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                /*您的业务逻辑 - 开始*/
+                System.out.println("hello world");
+                /*您的业务逻辑 - 结束*/
+            } catch (BlockException e1) {
+                /*流控逻辑处理 - 开始*/
+                System.out.println("block!");
+                /*流控逻辑处理 - 结束*/
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+        }
+    }
+
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(5);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+
+}
