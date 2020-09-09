633c5192bd3144b21371630686585c5f9a095fb6
king <king@qq.com>
2020-09-09 19:21:45 +0800

    add log demo

--- logback/springboot-log-group/src/main/java/com/kq/log/dao/LoginDao.java
+++ logback/springboot-log-group/src/main/java/com/kq/log/dao/LoginDao.java
@@ -3,6 +3,8 @@ package com.kq.log.dao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /**
  * @author kq
  * @date 2020-09-08 17:58
@@ -12,7 +14,10 @@ public class LoginDao {
 
     private Logger logger = LoggerFactory.getLogger(LoginDao.class);
 
+    private Logger transactionLogger = LoggerFactory.getLogger("transactionLogger");
+
     public void print(){
         logger.debug(" this is LoginDao print");
+        transactionLogger.info("本次扣款"+new Random().nextInt(10)+"元");
     }
 }
--- logback/springboot-log-group/src/main/java/com/kq/log/service/LoginService.java
+++ logback/springboot-log-group/src/main/java/com/kq/log/service/LoginService.java
@@ -3,6 +3,8 @@ package com.kq.log.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /**
  * @author kq
  * @date 2020-09-08 17:58
@@ -12,7 +14,10 @@ public class LoginService {
 
     private Logger logger = LoggerFactory.getLogger(LoginService.class);
 
+    private Logger transactionLogger = LoggerFactory.getLogger("transactionLogger");
+
     public void print(){
         logger.debug(" this is LoginService print");
+        transactionLogger.info("本次花费"+new Random().nextInt(10)+"元");
     }
 }
