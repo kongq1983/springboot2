76c0b47c0631719ad071a8432b8f6443facc67d4
king <kongq1983@qq.com>
2020-09-24 00:01:51 +0800

    add mybatis demo

--- common/src/main/java/com/kq/entity/School.java
+++ common/src/main/java/com/kq/entity/School.java
@@ -1,38 +1,45 @@
 package com.kq.entity;
 
+import lombok.Data;
+
 /**
  * School
  *
  * @author kq
  * @date 2019-11-27
  */
+
+@Data
 public class School {
 
     private Long id;
+    private String name;
     private Teacher teacher;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("School{");
-        sb.append("id=").append(id);
-        sb.append(", teacher=").append(teacher);
-        sb.append('}');
-        return sb.toString();
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Teacher getTeacher() {
+//        return teacher;
+//    }
+//
+//    public void setTeacher(Teacher teacher) {
+//        this.teacher = teacher;
+//    }
+//
+//
+//
+//    @Override
+//    public String toString() {
+//        final StringBuilder sb = new StringBuilder("School{");
+//        sb.append("id=").append(id);
+//        sb.append(", teacher=").append(teacher);
+//        sb.append('}');
+//        return sb.toString();
+//    }
 }
